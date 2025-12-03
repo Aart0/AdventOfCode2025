0b63f5bdb0ba65b498bd771af241e1b140cacfd2
Aart0 <71770493+Aart0@users.noreply.github.com>
2025-12-03 19:08:24 +0100

    finished the first half

--- Day_3.c
+++ Day_3.c
@@ -4,6 +4,9 @@
 
 #define MAX_LINE_LEN 150
 
+
+int glue_two_char(char a, char b);
+
 int main(int argc, char *argv[]){
     if (argc < 2){
         printf("Not enough arguments!\n");
@@ -20,26 +23,42 @@ int main(int argc, char *argv[]){
     char line[MAX_LINE_LEN];
 
     while(fgets(line, MAX_LINE_LEN, fp_in) != NULL){
+        line[strcspn(line, "\n")] = 0;
         int line_len = strlen(line);
-        char first = line[0], second = line[1]; 
-
-        for (int i = 2; i<line_len;i++){
-            printf("STEP: %d => first = %c, Second = %c, CURRENT CHAR = %c\n",i,first, second, line[i]);
+        char first = line[0], second = line[line_len-1];  
+        int max_num_pos = 0;
+        //printf("starting first = %c, starting second = %c\n\n",first, second);
+        for (int i = 1; i<line_len-1;i++){
             if(line[i] > first){
                 first = line[i];
-                continue;
+                max_num_pos = i;
             } 
-
-            if(line[i] > second){
-                second = line[i];
-                continue;
+        }
+        //printf("oh your first number is %c in the position of %d\n\n",first, max_num_pos);
+        for (int j = max_num_pos+1; j < line_len; j++){
+            if(line[j] > second){
+                second = line[j];
             }
         }
+        int num =  glue_two_char(first, second);
+        total_jolt += num;
+        printf("the FINAL number you got is %d\n",num);
     }
 
-    
-
+    printf("total output joltage is = %d",total_jolt);
     fclose(fp_in);
     return 0;
+}
+
+int glue_two_char(char a, char b){
+    int result;
+    char s[3];
+    
+    s[0] = a;
+    s[1] = b;
+    s[2] = '\0';
+    
+    result = atoi(s);
+    return result;
 }
 
